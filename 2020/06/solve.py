36a38c2d62f60fa88091d16d2637ad7e74d56ba2
Claes Hallström <hallstrom.claes@gmail.com>
2020-12-06 11:00:12 +0100

    Add 2020.06 solution

---
+++ 2020/06/solve.py
@@ -0,0 +1,26 @@
+"""Advent Of Code #06."""
+with open("input") as f:
+    data = f.read()
+
+groups = data.split("\n\n")
+groups = [group.splitlines() for group in groups]
+
+# Part 1
+answers = 0
+for group in groups:
+    group_answers = set()
+    for questions in group:
+        group_answers = group_answers.union(set(questions))
+    answers += len(group_answers)
+print("Part 1:", answers)
+assert answers == 6911
+
+# Part 2
+answers = 0
+for group in groups:
+    group_answers = set(group[0])
+    for questions in group[1:]:
+        group_answers = group_answers.intersection(set(questions))
+    answers += len(group_answers)
+print("Part 2:", answers)
+assert answers == 3473
