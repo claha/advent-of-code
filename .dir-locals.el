a9104068fdcf0e6e87b44e8b597b53004dd867dc
Claes Hallström <hallstrom.claes@gmail.com>
2020-12-09 10:26:48 +0100

    Update .dir-locals.el to also compile javascript

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-((nil . ((compile-command . "python solve.py"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js-mode . ((compile-command . "node solve.js")))
+ (python-mode . ((compile-command . "python solve.py"))))
