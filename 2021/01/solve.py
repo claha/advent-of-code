f14c489d979c6eb3da6d164cc609d54021caedf8
Claes Hallström <hallstrom.claes@gmail.com>
2021-12-01 13:32:15 +0100

    Add 2021.01 solution

---
+++ 2021/01/solve.py
@@ -0,0 +1,21 @@
+"""Advent Of Code #01."""
+with open("input") as f:
+    data = [int(d) for d in f.read().split()]
+
+
+# Part 1
+count = 0
+for i in range(1, len(data)):
+    if data[i] > data[i - 1]:
+        count += 1
+print("Part 1:", count)
+assert count == 1298
+
+
+# Part 2
+count = 0
+for i in range(3, len(data)):
+    if data[i] > data[i - 3]:
+        count += 1
+print("Part 2:", count)
+assert count == 1248
