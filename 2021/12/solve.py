474c841fcaae20fbbfeaef757f1f68dab8d58c23
Claes Hallström <hallstrom.claes@gmail.com>
2021-12-12 08:02:36 +0100

    Add 2021.12 solution

---
+++ 2021/12/solve.py
@@ -0,0 +1,44 @@
+"""Advent Of Code #12."""
+with open("input") as f:
+    data = [d.split("-") for d in f.read().split()]
+
+graph = {}
+for connection in data:
+    if not connection[0] in graph:
+        graph[connection[0]] = []
+    if not connection[1] in graph:
+        graph[connection[1]] = []
+    if connection[1] != "start":
+        graph[connection[0]].append(connection[1])
+    if connection[0] != "start":
+        graph[connection[1]].append(connection[0])
+
+
+def dfs(graph, path, revisited):
+    """Depth first search."""
+    global num_paths
+    node = path[-1]
+
+    if node == "end":
+        num_paths += 1
+        return
+
+    for neighbour in graph[node]:
+        if neighbour.lower() != neighbour or neighbour not in path:
+            dfs(graph, path + [neighbour], revisited)
+        elif not revisited:
+            dfs(graph, path + [neighbour], True)
+
+
+# Part 1
+num_paths = 0
+dfs(graph, ["start"], True)
+print("Part 1:", num_paths)
+assert num_paths == 5076
+
+
+# Part 2
+num_paths = 0
+dfs(graph, ["start"], False)
+print("Part 2:", num_paths)
+assert num_paths == 145643
