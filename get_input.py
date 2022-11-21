a3649e35be88ca6d5ef2ffdd955deee4b9bcdda0
Claes Hallström <hallstrom.claes@gmail.com>
2022-11-21 23:24:58 +0100

    Improve script to get input
    If no year/date given assume current year/day

--- get_input.py
+++ get_input.py
@@ -1,23 +1,34 @@
 """Get Advent Of Code input automatically."""
 import argparse
+import datetime
+import os
+import pathlib
 import urllib.request
 
 # Parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("year", type=int)
-parser.add_argument("day", type=int)
+parser.add_argument("year", type=int, nargs="?")
+parser.add_argument("day", type=int, nargs="?")
 args = parser.parse_args()
+
+# Setup default value to today if not specified
+today = datetime.datetime.now()
+if args.year is None:
+    args.year = today.year
+if args.day is None:
+    args.day = today.day
 
 # Create url and filename from arguments
 url = f"https://adventofcode.com/{args.year}/day/{args.day}/input"
 filename = f"{args.year}/{args.day:02}/input"
 
 # Read token from file and create cookies
-with open(".token", "r") as f:
+with open(".token") as f:
     token = f.read().strip()
 cookie = f"session={token}"
 
 # Fetch and save to file
 req = urllib.request.Request(url, headers={"Cookie": cookie.strip()})
+os.makedirs(pathlib.Path(filename).parent, exist_ok=True)
 with open(filename, "w") as f:
     f.write(urllib.request.urlopen(req).read().decode())
