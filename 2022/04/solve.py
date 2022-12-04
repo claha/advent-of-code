955db238d3dec021b12ebc9629cd3732171d532e
Claes Hallström <hallstrom.claes@gmail.com>
2022-12-04 08:52:27 +0100

    Add 2022.04 solution

---
+++ 2022/04/solve.py
@@ -0,0 +1,30 @@
+"""Advent Of Code #04."""
+with open("input") as f:
+    data = [
+        list(int(d) for d in line.split(","))
+        for line in f.read().replace("-", ",").split()
+    ]
+
+
+def contains(a, b, c, d):
+    """Check if a..b contain c..d or the other way around."""
+    return (c >= a and d <= b) or (a >= c and b <= d)
+
+
+def overlap(a, b, c, d):
+    """Check if a..b and c..d overlap."""
+    return max(a, c) <= min(b, d)
+
+
+# Part 1
+count = sum(contains(*abcd) for abcd in data)
+
+print("Part 1:", count)
+assert count == 494
+
+
+# Part 2
+count = sum(overlap(*abcd) for abcd in data)
+
+print("Part 2:", count)
+assert count == 833
